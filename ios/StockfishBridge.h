efbfc188af591b31075d7e8a3c9b080b5a5ae0d0
Chris Bouy <chrisbouy@gmail.com>
2024-12-01 19:00:36 -0600

    12 1 stockfish started integration

---
+++ ios/StockfishBridge.h
@@ -0,0 +1,12 @@
+//
+//  StockfishBridge.h
+//  ChessTutorAR
+//
+//  Created by Chris Bouy on 12/1/24.
+//
+
+#ifndef StockfishBridge_h
+#define StockfishBridge_h
+
+
+#endif /* StockfishBridge_h */
---
+++ ios/StockfishBridge.mm
@@ -0,0 +1,8 @@
+//
+//  StockfishBridge.m
+//  ChessTutorAR
+//
+//  Created by Chris Bouy on 12/1/24.
+//
+
+#import <Foundation/Foundation.h>

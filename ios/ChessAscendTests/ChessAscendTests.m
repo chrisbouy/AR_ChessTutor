cb4703cf74620af77b59bcfcb68b15054cab2ddd
Chris Bouy <chrisbouy@gmail.com>
2024-12-20 23:23:24 -0600

    splash screen works

--- ios/ChessAscendTests/ChessAscendTests.m
+++ ios/ChessAscendTests/ChessAscendTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface ChessTutorARTests : XCTestCase
+@interface ChessAscendTests : XCTestCase
 
 @end
 
-@implementation ChessTutorARTests
+@implementation ChessAscendTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
